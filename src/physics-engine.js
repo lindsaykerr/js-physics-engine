54960f7a9a9404248834fea512beed9cdd0f261b
Lindsay Kerr <wrenzenzen@gmail.com>
2022-05-27 18:08:48 +0100

    Used a more functional programming approach for the vectors

--- src/physics-engine.js
+++ src/physics-engine.js
@@ -1,2 +1,6 @@
-import * from './math/polyfills.js'
+//import * from './math/polyfills.js'
+import * as v from './math/vectorsv2.js'
 
+const diffVelocity = v.subtractVectors(v.Vec2(2,1), v.Vec2(2,3));
+const accelaration = v.
+const v1 = v.Vec1(23);
